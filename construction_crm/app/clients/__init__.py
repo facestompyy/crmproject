67409308a26c605063657cfa52f95348228cff12
facestompyy <naked.tunaaa@gmail.com>
2025-05-19 19:38:51 -0400

    Delete construction_crm directory

--- construction_crm/app/clients/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('clients', __name__)
-
-from app.clients import routes
--- construction_crm/app/main/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-from flask_login import login_required
-from app.main import bp
-
-@bp.route('/')
-@bp.route('/accueil')
-@login_required
-def index():
-    return render_template('main/index.html', title='Tableau de bord')
