dd9576f0ce31cd4592972f68455906fa392c075c
facestompyy <naked.tunaaa@gmail.com>
2025-05-19 20:07:32 -0400

    Delete construction_crm directory

--- construction_crm/app/Routes/Projets.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from app.extensions import db
-from app.models import Projet
-
-bp = Blueprint('projets', __name__)
-
-@bp.route('/projets')
-def list_projets():
-    return "Projects list"
--- construction_crm/app/Routes/Utilisateurs.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from app.extensions import db
-from app.models import Utilisateur
-
-bp = Blueprint('utilisateurs', __name__)
-
-@bp.route('/login')
-def login():
-    return "Login page"
